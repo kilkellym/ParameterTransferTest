b0b4327cb2755d59de3bd83178bb3614e0466929
kilkellym <michael@archsmarter.com>
2022-12-01 16:08:07 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace RevitAddin5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(618, 39);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(12, 81);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(618, 686);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 31;
+            this.listBox2.Location = new System.Drawing.Point(665, 81);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(618, 686);
+            this.listBox2.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(665, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(618, 39);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1302, 797);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Forms = System.Windows.Forms;
+
+namespace RevitAddin5
+{
+    public partial class Form1 : Forms.Form
+    {
+        public Document doc;
+        public Form1(Document _doc)
+        {
+            InitializeComponent();
+
+            doc = _doc;
+
+            comboBox1.Items.Add("Doors");
+            comboBox1.Items.Add("Walls");
+            comboBox2.Items.Add("Doors");
+            comboBox2.Items.Add("Walls");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            string selectCategory = comboBox1.SelectedItem as string;
+            List<string> paramNames = new List<string>();
+
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+
+            if (selectCategory == "Doors")
+            {
+                collector.OfCategory(BuiltInCategory.OST_Doors);         
+            }
+            else if(selectCategory == "Walls")
+            {
+                collector.OfCategory(BuiltInCategory.OST_Walls);
+            }
+
+            if(collector.Count() > 0)
+            {
+                paramNames = GetParameterNamesFromType(collector.ToList());
+
+                if (paramNames.Count > 0)
+                {
+                    foreach (string name in paramNames)
+                        listBox1.Items.Add(name);
+
+                }
+            }
+        }
+
+        private List<string> GetParameterNamesFromType(List<Element> elemList)
+        {
+            List<string> paramNames = new List<string>();
+
+            foreach (Element element in elemList)
+            {
+                foreach (Parameter curParam in element.Parameters)
+                {
+                    if (paramNames.Contains(curParam.Definition.Name) == false)
+                        paramNames.Add(curParam.Definition.Name);
+                }
+            }
+
+            paramNames.Sort();
+
+            return paramNames;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+
+            string selectCategory = comboBox2.SelectedItem as string;
+            List<string> paramNames = new List<string>();
+
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+
+            if (selectCategory == "Doors")
+            {
+                collector.OfCategory(BuiltInCategory.OST_Doors);
+            }
+            else if (selectCategory == "Walls")
+            {
+                collector.OfCategory(BuiltInCategory.OST_Walls);
+            }
+
+            if (collector.Count() > 0)
+            {
+                paramNames = GetParameterNamesFromType(collector.ToList());
+
+                if (paramNames.Count > 0)
+                {
+                    foreach (string name in paramNames)
+                        listBox2.Items.Add(name);
+
+                }
+            }
+        }
+    }
+}
